99605811a064bd7f1f742a7e6cdc59de0dc805fa
Jainam Shah <er.jainamshah16@gmail.com>
2018-01-27 08:55:47 -0600

    Javascript

---
+++ Factorial/app.js
@@ -0,0 +1,10 @@
+
+function factorialize(num) {
+var a=1;
+  for(b=1;b<=num;b++){
+    a=a*b;
+  }
+  return a;
+}
+
+factorialize(5);
---
+++ Reversestring/app.js
@@ -0,0 +1,10 @@
+
+function reverseString(str) {
+  var a = str.split("");
+  var b= a.reverse();
+
+  return b.join("");
+
+}
+
+reverseString("hello");
